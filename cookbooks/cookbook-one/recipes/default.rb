1a78d62c28f4159d4cb0a8239cb82586c79d330c
takeyan <takeyan.g@gmail.com>
2014-01-05 05:57:49 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/cookbook-one/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: cookbook-one
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello World! You should try Chef with RightScale!!"
---
+++ cookbooks/cookbook-one/recipes/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures intermediate"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "cookbook-one::default","Prints hello world output" 
